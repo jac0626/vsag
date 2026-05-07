23325c7e9e67282ab06d7ea50f714c77d9863bc4
JiangChao <jacllovey@qq.com>
2026-05-07 16:47:22 +0800

    ci: make sanitizer builds fail fast
    Signed-off-by: JiangChao <jacllovey@qq.com>

--- scripts/testing/test_parallel_by_name.sh
+++ scripts/testing/test_parallel_by_name.sh
@@ -23,13 +23,13 @@ fi
 for tag in ${parallel_tags}
 do
   othertag="~"${tag}${othertag}
-  logname="./log/"${tag}".log"
-  ./build/tests/${testname} -d yes ${UT_FILTER} -a --order rand --allow-running-no-tests ${tag} ${addition_tag} -o ${logname} 2>> ${logname} &
+  ./build/tests/${testname} -d yes ${UT_FILTER} -a --order rand --allow-running-no-tests ${tag} ${addition_tag} -o ./log/${tag}.log &
   pids+=($!)
+  logname="./log/"${tag}".log"
   logger_files+=($logname)
 done
 
-./build/tests/${testname} -d yes ${UT_FILTER} -a --order rand --allow-running-no-tests ${othertag} ${addition_tag} -o ./log/other.log 2>> ./log/other.log &
+./build/tests/${testname} -d yes ${UT_FILTER} -a --order rand --allow-running-no-tests ${othertag} ${addition_tag} -o ./log/other.log &
 pids+=($!)
 logger_files+=("./log/other.log")
 
@@ -52,21 +52,6 @@ do
   fi
   ((index+=1))
 done
-
-sanitizer_successful=true
-sanitizer_pattern='runtime error:|ERROR: (AddressSanitizer|UndefinedBehaviorSanitizer|LeakSanitizer)|SUMMARY: (AddressSanitizer|UndefinedBehaviorSanitizer|LeakSanitizer)'
-for log_file in "${logger_files[@]}"
-do
-  if grep -Eq "${sanitizer_pattern}" "${log_file}"; then
-    sanitizer_successful=false
-    echo "${log_file} sanitizer failed"
-    cat "${log_file}"
-  fi
-done
-
-if [ $sanitizer_successful = false ]; then
-  all_successful=false
-fi
 
 if [ $all_successful = true ]; then
   exit 0
